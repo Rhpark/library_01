2c322baab3fded116d4910a475da2ffb720a5115
RhPark <cli7942lic@gmail.com>
2024-11-09 14:05:03 +0900

    version   "0.5.1"
    Bug
     Edit
      1.
    
     Remove
      1.
    
    New
    
     Add
      1.DefaultToast 의 parameter context 사용 추가.
    
     Edit
      1.
    
     Remove
      1.
    
     Remains
      2. WindowManagerController 50%

--- app/src/main/java/kr/open/rhpark/app/activity/toast_snackbar/ToastSnackBarActivity.kt
+++ app/src/main/java/kr/open/rhpark/app/activity/toast_snackbar/ToastSnackBarActivity.kt
@@ -16,62 +16,52 @@ class ToastSnackBarActivity :
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        systemServiceManagerInfo.softKeyboardController.show(binding.editText,200L)
+        binding.run {
+            systemServiceManagerInfo.softKeyboardController.show(editText,200L)
 
-        binding.btnDefaultToast.setOnClickListener { toast.showMsgShort("Default Toast") }
+            btnDefaultToast.setOnClickListener { toast.showMsgShort("Default Toast") }
 
-        binding.btnCustomToast.visibility =
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) View.GONE
-            else View.VISIBLE
+            btnCustomToast.visibility = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) View.GONE else View.VISIBLE
+            btnCustomToast.setOnClickListener {
+                toast.setGravity(Triple(Gravity.CENTER_VERTICAL, 0, 0))
+                toast.setBackground(Color.YELLOW)
+                toast.showMsgShort("Custom Toast")
+            }
 
-        binding.btnCustomToast.setOnClickListener {
-            toast.setGravity(Triple(Gravity.CENTER_VERTICAL, 0, 0))
-            toast.setBackground(Color.YELLOW)
-            toast.showMsgShort("Custom Toast")
-        }
+            btnDefaultSnackBar.setOnClickListener { v -> snackBar.showMsgShort(v, "Default SnackBar") }
+            btnDefaultWindowViewSnackBar.setOnClickListener { snackBar.showMsgShortWindowView("Default Window View SnackBar") }
 
-        binding.btnDefaultSnackBar.setOnClickListener { v ->
-            snackBar.showMsgShort(v, "Default SnackBar")
-        }
-
-        binding.btnDefaultWindowViewSnackBar.setOnClickListener {
-            snackBar.showMsgShortWindowView("Default Window View SnackBar")
-        }
-
-        binding.btnCustomSnackBar.setOnClickListener { v ->
-
-            snackBar.apply {
-                setAnimation(animation = BaseTransientBottomBar.ANIMATION_MODE_SLIDE)
-                setTextColor(Color.BLUE)
-                setBackgroundTint(Color.WHITE)
-                setActionTextColor(Color.RED)
-                setAction("Action_01") { v ->
-                    toast.showMsgShort("OnClick SnackBar Action_01")
-                }
-            }.showMsgShort(v,"Custom SnackBar")
-        }
-
-        binding.btnAddConfigSnackBar.setOnClickListener { v->
-            snackBar.addSnackBarShortConfig(v,"short_01") { snackBar->
+            btnCustomSnackBar.setOnClickListener { v ->
                 snackBar.apply {
-                    animationMode = BaseTransientBottomBar.ANIMATION_MODE_SLIDE
-                    setTextColor(Color.WHITE)
-                    setBackgroundTint(Color.BLACK)
-                    setActionTextColor(Color.YELLOW)
-                    setAction("Short_01") { v ->
-                        toast.showMsgShort("OnClick SnackBar Short_01")
+                    setAnimation(animation = BaseTransientBottomBar.ANIMATION_MODE_SLIDE)
+                    setTextColor(Color.BLUE)
+                    setBackgroundTint(Color.WHITE)
+                    setActionTextColor(Color.RED)
+                    setAction("Action_01") { v ->
+                        toast.showMsgShort("OnClick SnackBar Action_01")
+                    }
+                }.showMsgShort(v,"Custom SnackBar")
+            }
+
+            btnAddConfigSnackBar.setOnClickListener { v->
+                snackBar.addSnackBarShortConfig(v,"short_01") { snackBar ->
+                    snackBar.apply {
+                        animationMode = BaseTransientBottomBar.ANIMATION_MODE_SLIDE
+                        setTextColor(Color.WHITE)
+                        setBackgroundTint(Color.BLACK)
+                        setActionTextColor(Color.YELLOW)
+                        setAction("Short_01") { v -> toast.showMsgShort("OnClick SnackBar Short_01") }
                     }
                 }
-                snackBar
+                toast.showMsgShort("Config Save SnackBar")
             }
-            toast.showMsgShort("Config Save SnackBar")
-        }
 
-        binding.btnShowConfigSnackBar.setOnClickListener {
-            snackBar.getSnackBarShortConfig("short_01")?.let {
-                it.setText("Hello Snack Short 01 ")
-                it.show()
-            }?: toast.showMsgShort("Config Not Found")
+            btnShowConfigSnackBar.setOnClickListener {
+                snackBar.getSnackBarShortConfig("short_01")?.let {
+                    it.setText("Hello Snack Short 01 ")
+                    it.show()
+                }?: toast.showMsgShort("Config Not Found!")
+            }
         }
     }
 }
--- library/src/main/java/kr/open/rhpark/library/ui/view/toast/DefaultToast.kt
+++ library/src/main/java/kr/open/rhpark/library/ui/view/toast/DefaultToast.kt
@@ -2,7 +2,6 @@ package kr.open.rhpark.library.ui.view.toast
 
 import android.content.Context
 import android.os.Build
-import android.view.View
 import android.widget.Toast
 
 public class DefaultToast(private val context: Context) {
@@ -43,31 +42,36 @@ public class DefaultToast(private val context: Context) {
         }
     }
 
+    /**
+     * Warning: Starting from Android Build. VERSION_CODES. R,
+     * for apps targeting API level Build. VERSION_CODES. R or higher,
+     * this method is a no-op when called on text toasts.
+     */
     public fun setBackground(defaultBackground: Int? = null) {
-       this.background = defaultBackground
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
+            this.background = defaultBackground
+        }
     }
 
     public fun showMsgShort(msg: String) { msgShort(msg).show() }
     public fun showMsgLong(msg: String) { msgLong(msg).show() }
     public fun showMsgDuration(msg: String, duration:Int) { msgDuration(msg, duration).show() }
 
-    public fun msgShort(msg: String): Toast = make(msg, Toast.LENGTH_SHORT)
-    public fun msgLong(msg: String): Toast = make(msg, Toast.LENGTH_LONG)
-    public fun msgDuration(msg: String,duration:Int): Toast = make(msg, duration)
+    public fun showMsgShort(context: Context, msg: String) { msgShort(context, msg).show() }
+    public fun showMsgLong(context: Context, msg: String) { msgLong(context, msg).show() }
+    public fun showMsgDuration(context: Context, msg: String, duration:Int) { msgDuration(context, msg, duration).show() }
 
-    private fun make(msg: String, duration: Int) = Toast.makeText(context, msg, duration).apply {
+    public fun msgShort(msg: String): Toast = make(context, msg, Toast.LENGTH_SHORT)
+    public fun msgLong(msg: String): Toast = make(context, msg, Toast.LENGTH_LONG)
+    public fun msgDuration(msg: String,duration:Int): Toast = make(context, msg, duration)
+
+    public fun msgShort(context: Context, msg: String): Toast = make(context, msg, Toast.LENGTH_SHORT)
+    public fun msgLong(context: Context, msg: String): Toast = make(context, msg, Toast.LENGTH_LONG)
+    public fun msgDuration(context: Context, msg: String,duration:Int): Toast = make(context, msg, duration)
+
+    private fun make(context: Context, msg: String, duration: Int) = Toast.makeText(context, msg, duration).apply {
         this@DefaultToast.margin?.let { this.setMargin(it.first, it.second) }
         this@DefaultToast.gravity?.let { this.setGravity(it.first, it.second, it.third) }
         this@DefaultToast.background?.let { this.view?.setBackgroundColor(it) }
-    }
-
-    public fun customShowShort(view: View) {
-        Toast(context)
-    }
-
-    private fun getCustomToast(view:View) = Toast(context).apply {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
-            this.view = view
-        }
     }
 }
